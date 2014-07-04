a1440c6fc3478ba1c23be2ee3017c8375d9cc77d
praktikant <praktikant@localhost>
2014-07-04 09:35:09 +0200

    Fixed NPE

--- org.evenos.broadcaster/WEB-INF/src/org/adempiere/webui/dashboard/DPBroadcaster.java
+++ org.evenos.broadcaster/WEB-INF/src/org/adempiere/webui/dashboard/DPBroadcaster.java
@@ -33,7 +33,9 @@ import org.zkoss.zul.Treeitem;
 import org.zkoss.zul.Treerow;
 import org.zkoss.zul.Vlayout;
 
-public class DPBroadcaster extends DashboardPanel implements EventListener<Event>, ITopicSubscriber<Map<String,Integer>> {//, EventHandler{
+public class DPBroadcaster extends DashboardPanel implements
+		EventListener<Event>, ITopicSubscriber<Map<String, Integer>> {// ,
+																		// EventHandler{
 
 	/**
 	 * 
@@ -41,7 +43,7 @@ public class DPBroadcaster extends DashboardPanel implements EventListener<Event
 	private static final long serialVersionUID = 3956606532708796545L;
 
 	private CLogger log = CLogger.getCLogger(DPBroadcaster.class);
-		
+
 	private Vlayout layout = new Vlayout();
 	private Div contentArea = new Div();
 	private Tree tree;
@@ -49,39 +51,43 @@ public class DPBroadcaster extends DashboardPanel implements EventListener<Event
 	private Div headerArea = new Div();
 	private Checkbox checkboxIsOnline = new Checkbox();
 	private Textbox filterUser = new Textbox();
-	
+
 	private Image about;
-	
+
 	private String oldFilterValue = "";
-	
+
 	private int ad_user_id = Env.getContextAsInt(Env.getCtx(), "#AD_User_ID");
-	
+
 	Desktop desktop = AEnv.getDesktop();
-		
+
 	private static final String ON_USER_LOGGED_IN_TOPIC = "onUserLoggedInTopic";
 	private static final String ON_USER_LOGGED_OUT_TOPIC = "onUserLoggedOutTopic";
-	
-	private String DPBroadcaster_User_Name = Msg.getMsg(Env.getCtx(), "DPBroadcaster_User_Name");
-	private String DPBroadcaster_Online_Users_Only = Msg.getMsg(Env.getCtx(), "DPBroadcaster_Online_Users_Only");
-	private String DPBroadcaster_Online_Users_Only_Tooltip = Msg.getMsg(Env.getCtx(), "DPBroadcaster_Online_Users_Only_Tooltip");
-	
+
+	private String DPBroadcaster_User_Name = Msg.getMsg(Env.getCtx(),
+			"DPBroadcaster_User_Name");
+	private String DPBroadcaster_Online_Users_Only = Msg.getMsg(Env.getCtx(),
+			"DPBroadcaster_Online_Users_Only");
+	private String DPBroadcaster_Online_Users_Only_Tooltip = Msg.getMsg(
+			Env.getCtx(), "DPBroadcaster_Online_Users_Only_Tooltip");
+
 	private ServerPushTemplate template;
 	private BroadcasterServerPushCallback callback;
-	class BroadcasterServerPushCallback implements IServerPushCallback{
+
+	class BroadcasterServerPushCallback implements IServerPushCallback {
 
 		BroadcasterTreeModel model;
-		public BroadcasterServerPushCallback(BroadcasterTreeModel model){
+
+		public BroadcasterServerPushCallback(BroadcasterTreeModel model) {
 			this.model = model;
 		}
-		
+
 		@Override
 		public void updateUI() {
 			model.refresh();
 		}
-		
+
 	}
-	
-	
+
 	public DPBroadcaster() {
 		super();
 
@@ -90,19 +96,22 @@ public class DPBroadcaster extends DashboardPanel implements EventListener<Event
 
 		initLayout();
 		initComponents();
-		
-		IMessageService service = Service.locator().locate(IMessageService.class).getService();
+
+		IMessageService service = Service.locator()
+				.locate(IMessageService.class).getService();
 		if (service != null) {
-			ITopic<Map<String,Integer>> intopic = service.getTopic(ON_USER_LOGGED_IN_TOPIC);
+			ITopic<Map<String, Integer>> intopic = service
+					.getTopic(ON_USER_LOGGED_IN_TOPIC);
 			intopic.subscribe(this);
-			
-			ITopic<Map<String,Integer>> outtopic = service.getTopic(ON_USER_LOGGED_OUT_TOPIC);
+
+			ITopic<Map<String, Integer>> outtopic = service
+					.getTopic(ON_USER_LOGGED_OUT_TOPIC);
 			outtopic.subscribe(this);
 		}
-		
+
 		this.template = new ServerPushTemplate(desktop);
 		this.callback = new BroadcasterServerPushCallback(treeModel);
-					
+
 	}
 
 	private void initLayout() {
@@ -125,11 +134,10 @@ public class DPBroadcaster extends DashboardPanel implements EventListener<Event
 		contentArea.setStyle("margin:5px 5px;overflow: auto;");
 	}
 
-	
 	private void initComponents() {
-		
+
 		treeModel = BroadcasterTreeModel.create(ad_user_id, this, false);
-		
+
 		tree = new Tree();
 		tree.setMultiple(false);
 		tree.setWidth("100%");
@@ -139,128 +147,142 @@ public class DPBroadcaster extends DashboardPanel implements EventListener<Event
 		tree.setItemRenderer(treeModel);
 		tree.setStyle("border: none");
 		tree.addEventListener(Events.ON_CLICK, this);
-		
+
 		contentArea.appendChild(tree);
 
 		checkboxIsOnline.addEventListener(Events.ON_CLICK, this);
-		checkboxIsOnline.setTooltiptext(DPBroadcaster_Online_Users_Only_Tooltip);
-		
-		filterUser.addEventListener(Events.ON_BLUR, this);	
+		checkboxIsOnline
+				.setTooltiptext(DPBroadcaster_Online_Users_Only_Tooltip);
+
+		filterUser.addEventListener(Events.ON_BLUR, this);
 		filterUser.addEventListener(Events.ON_OK, this);
 		filterUser.setHflex("1");
-		
-		
-		about = new Image(ThemeManager.getThemeResource("images/InfoIndicator16.png"));
+
+		about = new Image(
+				ThemeManager.getThemeResource("images/InfoIndicator16.png"));
 		about.addEventListener(Events.ON_CLICK, this);
 		about.setStyle("cursor: pointer;");
-		
-		//Add Combobox and Checkbox to header area
+
+		// Add Combobox and Checkbox to header area
 		Label labelUserFilter = new Label(DPBroadcaster_User_Name);
 		Label labelIsOnline = new Label(DPBroadcaster_Online_Users_Only);
-		
+
 		Hbox box = new Hbox();
 		box.setHflex("1");
-		box.setStyle("margin:5px 5px;");	
+		box.setStyle("margin:5px 5px;");
 		box.appendChild(labelUserFilter);
 		box.appendChild(filterUser);
 		box.appendChild(labelIsOnline);
 		box.appendChild(checkboxIsOnline);
 		box.appendChild(about);
 		headerArea.appendChild(box);
-		
-	}
 
+	}
 
 	@Override
 	public void onEvent(Event event) throws Exception {
 		Component comp = event.getTarget();
-        String eventName = event.getName();
-        
-		if(eventName.equals(Events.ON_CLICK)){
-			if(comp.equals(checkboxIsOnline)){
-				log.fine("Changed online Users only: " + checkboxIsOnline.isSelected());	
+		String eventName = event.getName();
+
+		if (eventName.equals(Events.ON_CLICK)) {
+			if (comp.equals(checkboxIsOnline)) {
+				log.fine("Changed online Users only: "
+						+ checkboxIsOnline.isSelected());
 
 				treeModel.setOnlyOnlineUsers(checkboxIsOnline.isSelected());
-		
-			}else if (comp instanceof Treerow) 
-			{
+
+			} else if (comp instanceof Treerow) {
 				Treeitem selectedItem = (Treeitem) comp.getParent();
-				Object attribue = selectedItem.getAttribute(BroadcasterTreeModel.ATTR_TREE_NODE);
-				if(attribue instanceof BroadcasterTreeNode<?>){
+				Object attribue = selectedItem
+						.getAttribute(BroadcasterTreeModel.ATTR_TREE_NODE);
+				if (attribue instanceof BroadcasterTreeNode<?>) {
 					@SuppressWarnings("unchecked")
-					BroadcasterTreeNode<Object> node = (BroadcasterTreeNode<Object>)attribue;
-					if(treeModel.isObjectOpened(node))
+					BroadcasterTreeNode<Object> node = (BroadcasterTreeNode<Object>) attribue;
+					if (treeModel.isObjectOpened(node))
 						treeModel.removeOpenObject(node);
 					else
 						treeModel.addOpenObject(node);
-					}			
-			}else if(comp.equals(about)){
+				}
+			} else if (comp.equals(about)) {
 				BroadcasterAboutWindow w = new BroadcasterAboutWindow();
 				w.setPage(this.getPage());
 				w.doHighlighted();
-			}else{
+			} else {
 				doOnClick(comp);
 			}
-		} else if (event.getName().equals(Events.ON_BLUR) || event.getName().equals(Events.ON_OK)) {
-			
+		} else if (event.getName().equals(Events.ON_BLUR)
+				|| event.getName().equals(Events.ON_OK)) {
+
 			filterUser.setValue(filterUser.getValue().trim());
-			if(!filterUser.getValue().equals(oldFilterValue)){
+			if (!filterUser.getValue().equals(oldFilterValue)) {
 				treeModel.setFilterUserName(filterUser.getValue());
-				oldFilterValue = filterUser.getValue(); 
+				oldFilterValue = filterUser.getValue();
 			}
-		} 
+		}
 
 	}
-	
+
 	private void doOnClick(Component acomp) {
 		Component comp = null;
-		if(acomp.getParent().getParent().getParent() instanceof Treeitem)
-			comp = acomp.getParent().getParent().getParent();
-		
-		
-		BroadcasterMessageWindow w = new BroadcasterMessageWindow(this.ad_user_id,
-				(Integer)comp.getAttribute(BroadcasterTreeModel.ATTR_AD_CLIENT_ID),
-				(Integer)comp.getAttribute(BroadcasterTreeModel.ATTR_AD_ORG_ID),
-				(Integer)comp.getAttribute(BroadcasterTreeModel.ATTR_AD_ROLE_ID),
-				(Integer)comp.getAttribute(BroadcasterTreeModel.ATTR_AD_USER_ID),
-				(Integer)comp.getAttribute(BroadcasterTreeModel.ATTR_C_COUNTRY_ID),
-				(Integer)comp.getAttribute(BroadcasterTreeModel.ATTR_C_REGION_ID)
-		);
-		w.setPage(this.getPage());
-		w.doHighlighted();
+		try {
+			if (acomp.getParent().getParent().getParent() instanceof Treeitem)
+				comp = acomp.getParent().getParent().getParent();
 
+			if (comp != null) {
+				BroadcasterMessageWindow w = new BroadcasterMessageWindow(
+						this.ad_user_id,
+						(Integer) comp
+								.getAttribute(BroadcasterTreeModel.ATTR_AD_CLIENT_ID),
+						(Integer) comp
+								.getAttribute(BroadcasterTreeModel.ATTR_AD_ORG_ID),
+						(Integer) comp
+								.getAttribute(BroadcasterTreeModel.ATTR_AD_ROLE_ID),
+						(Integer) comp
+								.getAttribute(BroadcasterTreeModel.ATTR_AD_USER_ID),
+						(Integer) comp
+								.getAttribute(BroadcasterTreeModel.ATTR_C_COUNTRY_ID),
+						(Integer) comp
+								.getAttribute(BroadcasterTreeModel.ATTR_C_REGION_ID));
+				w.setPage(this.getPage());
+				w.doHighlighted();
+			}
+		} catch (Exception e) {
+			// DO Nothing
+		}
 	}
-	
 
 	@Override
-	public void onMessage(Map<String,Integer> message) {
-		
-		try{
-			if(message.keySet().contains(ON_USER_LOGGED_IN_TOPIC)){
+	public void onMessage(Map<String, Integer> message) {
+
+		try {
+			if (message.keySet().contains(ON_USER_LOGGED_IN_TOPIC)) {
 				treeModel.userLoggedIn(message.get(ON_USER_LOGGED_IN_TOPIC));
-			}else if(message.keySet().contains(ON_USER_LOGGED_OUT_TOPIC))
+			} else if (message.keySet().contains(ON_USER_LOGGED_OUT_TOPIC))
 				treeModel.userLoggedOut(message.get(ON_USER_LOGGED_OUT_TOPIC));
-		}catch(Exception e){
-			log.severe("Error during update of BroadcasterTreeModel: "+e);			
+		} catch (Exception e) {
+			log.severe("Error during update of BroadcasterTreeModel: " + e);
 		}
 
 		template.executeAsync(callback);
-		
+
 	}
-	
+
 	@Override
 	public void detach() {
 
-		IMessageService service = Service.locator().locate(IMessageService.class).getService();
+		IMessageService service = Service.locator()
+				.locate(IMessageService.class).getService();
 		if (service != null) {
-			ITopic<Map<String,Integer>> intopic = service.getTopic(ON_USER_LOGGED_IN_TOPIC);
+			ITopic<Map<String, Integer>> intopic = service
+					.getTopic(ON_USER_LOGGED_IN_TOPIC);
 			intopic.unsubscribe(this);
-			
-			ITopic<Map<String,Integer>> outtopic = service.getTopic(ON_USER_LOGGED_OUT_TOPIC);
+
+			ITopic<Map<String, Integer>> outtopic = service
+					.getTopic(ON_USER_LOGGED_OUT_TOPIC);
 			outtopic.unsubscribe(this);
 		}
 		super.detach();
-		
+
 	}
 
 }
